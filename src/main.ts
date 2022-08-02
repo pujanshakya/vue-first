a88de4f82619fe2bdfdedaa7f9e187a541c90478
Pujan Shaya <pujanshakya18@gmail.com>
2022-08-02 19:19:20 +0545

    init: Initialized

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import "./assets/sb-admin/vendor/fontawesome-free/css/all.min.css";
+import "./assets/sb-admin/css/sb-admin-2.min.css";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    //...
+  ],
+});
+
+// import "./assets/sb-admin/vendor/jquery/jquery.min.js";
+// import "./assets/sb-admin/vendor/bootstrap/js/bootstrap.bundle.min.js";
+// import "./assets/sb-admin/vendor/jquery-easing/jquery.easing.min.js";
+// import "./assets/sb-admin/js/sb-admin-2.min.js";
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
